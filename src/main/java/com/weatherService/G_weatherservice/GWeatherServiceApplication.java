36c1727e6389cb5875343e850bcbc07ecf5238c6
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-13 12:09:57 +0530

    hystrix service

---
+++ src/main/java/com/weatherService/G_weatherservice/GWeatherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.weatherService.G_weatherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GWeatherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GWeatherServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weatherService/G_weatherservice/GWeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherService.G_weatherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GWeatherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
